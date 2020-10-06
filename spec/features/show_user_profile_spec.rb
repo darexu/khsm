607e2c7f975cdc74851b023b06c6638daf6ebd0b
darexu <darexr.u@mail.ru>
2020-10-06 05:03:08 +0300

    show user prifile spec

---
+++ spec/features/show_user_profile_spec.rb
@@ -0,0 +1,51 @@
+# Как и в любом тесте, подключаем помощник rspec-rails
+require 'rails_helper'
+
+RSpec.feature "A user is looking at another user's profile", type: :feature do
+
+  let(:user) {  FactoryBot.create :user, name: 'Юра' }
+
+  let!(:games) do
+    [
+      FactoryBot.create(:game_with_questions, user: user, created_at: '2020.10.06, 10:00',
+                        finished_at: '2020.10.06, 10:10', current_level: 2, is_failed: true,
+                        prize: 27000),
+      FactoryBot.create(:game_with_questions, user: user, created_at: '2020.10.02, 10:00',
+                        finished_at: '2020.10.06, 10:10', current_level: 16, is_failed: false,
+                        prize: 1000000),
+      FactoryBot.create(:game_with_questions, user: user, created_at: '2020.10.03, 12:00',
+                        finished_at: '2020.10.03, 10:10', current_level: 1, is_failed: true,
+                        prize: 0)
+    ]
+
+  end
+
+  scenario 'successfully' do
+    visit '/'
+
+    click_link 'Юра'
+
+    expect(page).to have_current_path '/users/1'
+    expect(page).not_to have_content('Сменить имя и пароль')
+
+    expect(page).to have_content('1')
+    expect(page).to have_content('проигрыш')
+    expect(page).to have_content('06 окт., 10:00')
+    expect(page).to have_content('27 000 ₽')
+
+    expect(page).to have_content('2')
+    expect(page).to have_content('проигрыш')
+    expect(page).to have_content('03 окт., 12:00')
+    expect(page).to have_content('0 ₽')
+
+    expect(page).to have_content('3')
+    expect(page).to have_content('победа')
+    expect(page).to have_content('02 окт., 10:00')
+    expect(page).to have_content('16')
+    expect(page).to have_content('1 000 000 ₽')
+    # В процессе работы можно использовать
+    # save_and_open_page
+    # но в конечном коде (который вы кладете в репозиторий)
+    # этого кода быть не должно, также, как и byebug
+  end
+end
